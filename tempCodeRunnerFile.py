3b5e89e08cd6d28531f516050ff5b740c27f69ec
HanX-gthb <94861831+HanX-gthb@users.noreply.github.com>
2022-05-27 00:37:14 +0800

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-        # pos1, pos2 = x.index("("), x.index(")")
-        # code =x [pos1 + 1 :pos2] #取股票代码,如600039
-        # ur1 = "https://quote.eastmoney.com/sh" + code + ".html"
-        # await page.goto (ur1)
-        # html = await page. content() #往下编程前可以先print (html)看一看
-        # pt = '<td>([^<]*)</td>. *?<td[^>]*id= ="gt\d*?"[^>]*>([^<]*)</td> '
-        # for x in re. finda1l (pt,html, re.DOTALL) :
-        #     print(x[0] ,x[1])
